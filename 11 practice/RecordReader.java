e1214271c075607441a2ae8a13298de9e504ddb8
famalone <finmalone@gmail.com>
2015-03-17 11:25:48 -0500

    recursive stuff

---
+++ 11 practice/RecordReader.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+/**
+   This class processes baby name records.
+*/
+public class RecordReader
+{
+   private double total;
+   private double limit;
+
+   /**
+      Constructs a RecordReader with a zero total.
+   */
+   public RecordReader(double aLimit)
+   {
+      total = 0;
+      limit = aLimit;
+   }
+
+   /**
+      Reads an input record and prints the name if the current total is less 
+      than the limit.
+      @param in the input stream
+   */
+   public void process(Scanner in)
+   {
+      String name = in.next();
+      int count = in.nextInt() * 1000;
+      System.out.print(count);
+      count += in.nextInt();
+      
+      if (total < limit) { System.out.print(name + " "); }
+      total++;
+   }
+
+   /**
+      Checks whether there are more inputs to process
+      @return true if the limit has not yet been reached
+   */
+   public boolean hasMore()
+   {
+      return total < limit;
+   }
+}
