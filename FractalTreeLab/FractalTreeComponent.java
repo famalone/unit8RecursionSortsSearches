e252de8e5bf17737eaee95ce00c599058a2c0c38
famalone <finmalone@gmail.com>
2015-03-25 11:26:42 -0500

    fractal

--- FractalTreeLab/FractalTreeComponent.java
+++ FractalTreeLab/FractalTreeComponent.java
@@ -9,33 +9,52 @@ import javax.swing.JComponent;
  */
 public class FractalTreeComponent extends JComponent
 {
-    private final int PANEL_WIDTH = 2000;
-    private final int PANEL_HEIGHT = 1100;
+    private final int PANEL_WIDTH = 1500;
+    private final int PANEL_HEIGHT = 1000;
 
-    private final int START_X = 1000;
-    private final int START_Y = 950;
+    private final int START_X = 750;
+    private final int START_Y = 900;
 
     private int current = 1;
+    private int angle = 30;
+    private final double LINE_FACTOR = .8;
+
     /**
      * Default constructor for objects of class FractalTreeComponent
      */
     public FractalTreeComponent()
     {
-        setBackground(Color.blue);
-        setPreferredSize (new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
+        this.setBackground(Color.blue);
+        this.setPreferredSize (new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
     }
-    
-    public void drawFractal(int order,int x1,int y1, int x2, int y2, Graphics page)
+
+    public void drawFractal(int order, int x1, int y1, int x2, int y2,
+    Graphics page, int previousAngle)
     {
+        int distX, distY;
         if(order == 1)
+        {
             page.drawLine(x1, y1, x2, y2);
-        else if(order >= 500)
+            current++;
+        }
+        else if(order >= 200)
         {
             return;
         }
         else
         {
+            angle = previousAngle + angle;
             
+            current++;
         }
+    }
+
+    public void paintComponent (Graphics page)
+    {
+        super.paintComponent (page);
+
+        page.setColor (Color.green);
+
+        drawFractal (current, START_X, START_Y, START_X,START_Y - 100 , page, angle);
     }
 }
--- FractalTreeLab/FractalTreeViewer.java
+++ FractalTreeLab/FractalTreeViewer.java
@@ -1,4 +1,5 @@
 import javax.swing.JFrame;
+import java.awt.Color;
 
 /**
  * Write a description of class FractalTreeViewer here.
@@ -8,14 +9,18 @@ import javax.swing.JFrame;
  */
 public class FractalTreeViewer
 {
-    JFrame frame = new JFrame();
-    
-    frame.setSize(2000, 1100);
-    frame.setTitle("Fractal Tree!!");
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    
-    FractalTreeComponent component = new FractalTreeComponent();
-    frame.add(component);
-    
-    frame.setVisible(true);
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame();
+
+        frame.setSize(1500, 1000);
+        frame.setTitle("Fractal Tree!!");
+        frame.setBackground(Color.blue);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        FractalTreeComponent component = new FractalTreeComponent();
+        frame.add(component);
+
+        frame.setVisible(true);
+    }
 }
